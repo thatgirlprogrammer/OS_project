55c5b6e8fed94e7a8d3cdd95917dad6b9a765fa9
frozen-in-sunlight <72165321+frozen-in-sunlight@users.noreply.github.com>
2020-10-26 09:37:36 -0400

    Create driver.cpp

---
+++ OS_project-main/ConsoleApplication1/ConsoleApplication1/driver.cpp
@@ -0,0 +1,12 @@
+#include "driver.h"
+
+namespace OSSim {
+	driver::driver() {
+		// loader();
+		while (true) {
+			long_term_scheduler long_term;
+			short_term_scheduler short_term;
+			CPU cpu;
+		}
+	}
+}
