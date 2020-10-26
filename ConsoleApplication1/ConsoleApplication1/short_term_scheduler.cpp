aad463d2cafc525632b60e7b88e3da1c48da884f
frozen-in-sunlight <72165321+frozen-in-sunlight@users.noreply.github.com>
2020-10-26 19:57:34 -0400

    Update and rename short_term_schedular.cpp to short_term_scheduler.cpp

---
+++ ConsoleApplication1/ConsoleApplication1/short_term_scheduler.cpp
@@ -0,0 +1,22 @@
+#include "short_term_scheduler.h"
+
+OSSim::short_term_scheduler::short_term_scheduler(IRunnable* queue)
+{
+	runnable_process = queue;
+}
+
+OSSim::short_term_scheduler::~short_term_schedular() {
+	delete runnable_process;
+	runnable_process = NULL;
+}
+
+void OSSim::short_term_scheduler::add_job(PCB_info info, std::vector<Instruction>* instructions)
+{
+	runnable_process->add_job(info, instructions);
+}
+
+void OSSim::short_term_scheduler::execute_job()
+{
+	runnable_process->run_job();
+}
+
