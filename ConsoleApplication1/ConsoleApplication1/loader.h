df1a495b3c950c2f0a3b8a2dc6763ac7e2d9fb25
thatgirlprogrammer <mullins.amy@gmail.com>
2020-10-27 09:59:59 -0400

    Update loader.h

--- ConsoleApplication1/ConsoleApplication1/loader.h
+++ ConsoleApplication1/ConsoleApplication1/loader.h
@@ -3,15 +3,19 @@
 #include <string>
 #include <fstream>
 #include "disk.h"
-using namespace OSSim;
+#include "pcb.h"
+
 using namespace std;
+using namespace OSSim;
+
 class loader
 {
 public:
 	loader(string file_name);
 	void load_file();
+	PCB_info get_info();
 private: 
 	fstream file;
 	disk dsk;
+	PCB_info info;
 };
-
