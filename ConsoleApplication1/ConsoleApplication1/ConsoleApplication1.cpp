c2328b11db17c6b3855acf75f72e476c5252b76a
thatgirlprogrammer <mullins.amy@gmail.com>
2020-10-26 20:59:34 -0400

    Update ConsoleApplication1.cpp

--- ConsoleApplication1/ConsoleApplication1/ConsoleApplication1.cpp
+++ ConsoleApplication1/ConsoleApplication1/ConsoleApplication1.cpp
@@ -70,6 +70,34 @@ int main() {
 	v.push_back(Instruction(0xC10000AC));
 	v.push_back(Instruction(0x92000000));
 	disassemble(v);
+	
+	std::vector<int> v2;
+	v.push_back(0xC050005C);
+	v.push_back(0x4B060000);
+	/*v.push_back(Instruction(0x4B010000));
+	v.push_back(Instruction(0x4B000000));
+	v.push_back(Instruction(0x4F0A005C));
+	v.push_back(Instruction(0x4F0D00DC));
+	v.push_back(Instruction(0x4C0A0004));
+	v.push_back(Instruction(0xC0BA0000));
+	v.push_back(Instruction(0x42BD0000));
+	v.push_back(Instruction(0x4C0D0004));
+	v.push_back(Instruction(0x4C060001));
+	v.push_back(Instruction(0x10658000));
+	v.push_back(Instruction(0x56810018));
+	v.push_back(Instruction(0x4B060000));
+	v.push_back(Instruction(0x4F0900DC));
+	v.push_back(Instruction(0x43970000));
+	v.push_back(Instruction(0x05070000));
+	v.push_back(Instruction(0x4C060001));
+	v.push_back(Instruction(0x4C090004));
+	v.push_back(Instruction(0x10658000));
+	v.push_back(Instruction(0x5681003C));
+	v.push_back(Instruction(0xC10000AC));
+	v.push_back(Instruction(0x92000000));*/
+	CPU* cpu1 = new CPU();
+	cpu1->load_into_ram(&v2, info);
+	cpu1->step();
 
 	return 0;
 }
