284cce9f97d9de8e924bbd1bb864a59c02d696c7
frozen-in-sunlight <72165321+frozen-in-sunlight@users.noreply.github.com>
2020-10-29 14:11:43 -0400

    Update CPU.h

--- ConsoleApplication1/ConsoleApplication1/CPU.h
+++ ConsoleApplication1/ConsoleApplication1/CPU.h
@@ -2,6 +2,7 @@
 
 #include "instruction.h"
 #include "Memory.h"
+#include "short_term_scheduler.h"
 
 class CPU
 {
@@ -14,7 +15,9 @@ public:
 	void step();
 
 	uint32_t getPC() { return this->pc; }
+	void setPC() { pc = 0; }
 	bool isDone() { return this->done; }
+	void setDone() { this->done = false; }
 private:
 	int32_t registers[REGISTER_COUNT];
 	uint32_t pc;
