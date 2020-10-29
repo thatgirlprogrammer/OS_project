cb70011def3201af44d50be797671f9442869f74
frozen-in-sunlight <72165321+frozen-in-sunlight@users.noreply.github.com>
2020-10-28 22:37:24 -0400

    Update loader.h

--- ConsoleApplication1/ConsoleApplication1/loader.h
+++ ConsoleApplication1/ConsoleApplication1/loader.h
@@ -2,6 +2,7 @@
 
 #include <string>
 #include <fstream>
+#include <vector>
 #include "disk.h"
 #include "pcb.h"
 
@@ -13,9 +14,9 @@ class loader
 public:
 	loader(string file_name, disk* d);
 	void load_file();
-	PCB_info get_info();
+	PCB_info get_info(int index);
 private: 
 	fstream file;
 	disk* dsk;
-	PCB_info info;
+	vector<PCB_info> collection;
 };
