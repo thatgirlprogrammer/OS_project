0ac6febe27cc382c432d9774a45bc1b32da17077
frozen-in-sunlight <72165321+frozen-in-sunlight@users.noreply.github.com>
2020-10-28 22:37:49 -0400

    Update long_term_scheduler.h

--- ConsoleApplication1/ConsoleApplication1/long_term_scheduler.h
+++ ConsoleApplication1/ConsoleApplication1/long_term_scheduler.h
@@ -2,19 +2,23 @@
 #include <vector>
 #include "pcb.h"
 #include "ram.h"
+#include "disk.h"
 
 namespace OSSim
 {
 	class long_term_scheduler
 	{
 	public:
-		long_term_scheduler(RAM* ram) { ram1 = ram; }
+		long_term_scheduler() { ram1 = new RandomAccess; }
 		~long_term_scheduler() { delete ram1; ram1 = nullptr; }
-		void write_to_ram(uint8_t hex[], int start, int end, uint8_t* process, PCB_info pcb1) {
-			ram1->store(hex, start, end, process, pcb1);
+		void write_to_ram(int32_t hex, int start) {
+			ram1->store(hex, start);
 		}
-		RAM get_RAM() { return *ram1; }
+		int32_t read(int index) {
+			return ram1->pass(index);
+		}
+		RandomAccess get_RAM() { return *ram1; }
 	private:
-		RAM* ram1;
+		RandomAccess* ram1;
 	};
 }
