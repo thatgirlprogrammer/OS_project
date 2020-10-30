0d116f1e671e9b5622ab44d93bb489174e47a468
frozen-in-sunlight <72165321+frozen-in-sunlight@users.noreply.github.com>
2020-10-29 22:27:47 -0400

    Update short_term_scheduler.cpp

--- Updated Build 2.0/OS_project-main - Copy/ConsoleApplication1/ConsoleApplication1/short_term_scheduler.cpp
+++ Updated Build 2.0/OS_project-main - Copy/ConsoleApplication1/ConsoleApplication1/short_term_scheduler.cpp
@@ -1,1 +1,17 @@
 #include "short_term_scheduler.h"
+
+void short_term_scheduler::schedule() {
+	if (ready->size() > 0) {
+		PCB_info* process = ready->at(0);
+		uint16_t b = process->pc.job_memory_address;
+		std::cout << "This is job " << process->pc.job_number << endl;
+		cpu->setBase(b);
+		uint16_t j = 0;
+		for (int i = 0; i < process->pc.job_size; ++i) {
+			mem->deallocate(j + (b * 4));
+			j += 4;
+		}
+		ready->erase(ready->begin() + 0);
+		std::cout << "My job size is " << process->pc.job_size << std::endl;
+	}
+}
