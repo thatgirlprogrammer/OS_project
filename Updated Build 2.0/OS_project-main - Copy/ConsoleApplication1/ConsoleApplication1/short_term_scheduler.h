eff3625928dd9633346449732d48c82d0132de8a
frozen-in-sunlight <72165321+frozen-in-sunlight@users.noreply.github.com>
2020-10-29 22:27:27 -0400

    Update short_term_scheduler.h

--- Updated Build 2.0/OS_project-main - Copy/ConsoleApplication1/ConsoleApplication1/short_term_scheduler.h
+++ Updated Build 2.0/OS_project-main - Copy/ConsoleApplication1/ConsoleApplication1/short_term_scheduler.h
@@ -2,23 +2,39 @@
 #include <vector>
 #include "runnable.h"
 #include "Memory.h"
+#include "pcb.h"
+#include "loader.h"
+#include "CPU.h"
 
 namespace OSSim {
 	static void schedule() {}
 
 	class short_term_scheduler {
 	public:
-		short_term_scheduler(Memory* memory)
+		short_term_scheduler(Memory* memory, loader* ld, CPU* c)
 		{
 			mem = memory;
+			load = ld;
+			cpu = c;
+			ready = ld->get_ready();
+			waiting = ld->get_waiting();
+			running = ld->get_running();
+			terminated = ld->get_terminated();
 		}
 		~short_term_scheduler() {
 			delete mem; mem = NULL;
 		}
-		void add_memory(uint16_t index, int32_t value) {
-			mem->setMem(index * 4, value);
-		}
+		void schedule();
+		//void add_memory(uint16_t index, int32_t value) {
+		//	mem->setMem(index * 4, value);
+		//}
 	private:
 		Memory* mem;
+		CPU* cpu;
+		loader* load;
+		vector<PCB_info*>* ready;
+		vector<PCB_info*>* waiting;
+		vector<PCB_info*>* running;
+		vector<PCB_info*>* terminated;
 	};
 }
