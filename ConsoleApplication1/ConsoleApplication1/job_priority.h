a56278fe8631b43e0ac563d739d6e4bd5227d667
thatgirlprogrammer <mullins.amy@gmail.com>
2020-11-03 09:41:48 -0500

    Update job_priority.h

--- ConsoleApplication1/ConsoleApplication1/job_priority.h
+++ ConsoleApplication1/ConsoleApplication1/job_priority.h
@@ -16,13 +16,6 @@ namespace OSSim {
 			std::sort(compare.begin(), compare.end());
 		}
 
-		void print_compare() {
-			for (int i = 0; i < compare.size(); i++) {
-				auto val = compare.at(i);
-				std::cout << "job priority " << val.get_pcb_info().pc.job_priority << std::endl;
-			}
-		}
-
 		PCB_info get_pcb(int index) {
 			return compare.at(index).get_pcb_info();
 		}
