cdfc3024ed210f1f1df4529b4bddc021e667c2db
thatgirlprogrammer <mullins.amy@gmail.com>
2020-11-03 09:28:57 -0500

    Update Memory.cpp

--- ConsoleApplication1/ConsoleApplication1/Memory.cpp
+++ ConsoleApplication1/ConsoleApplication1/Memory.cpp
@@ -6,8 +6,6 @@
 int32_t Memory::getMem(uint16_t addr) {
 	mtx.lock();
 	assert(addr % 4 == 0);
-
-	// TODO: get offset from PCB
 	int32_t result = this->memory[addr] << 24;
 	result |= this->memory[addr + 1] << 16;
 	result |= this->memory[addr + 2] << 8;
@@ -18,17 +16,13 @@ int32_t Memory::getMem(uint16_t addr) {
 
 bool Memory::isOccupied(uint16_t addr) {
 	assert(addr % 4 == 0);
-
 	bool result = this->free[addr];
-
 	return result;
 }
 
 void Memory::setMem(uint16_t addr, int32_t data) {
 	mtx.lock();
 	assert(addr % 4 == 0);
-
-	// TODO: get offset from PCB
 	this->memory[addr] = data >> 24;
 	this->memory[addr + 1] = data >> 16;
 	this->memory[addr + 2] = data >> 8;
@@ -71,13 +65,10 @@ std::string Memory::dump() {
 	output << "      00       04       08       0c" << std::endl;
 	for (int i = 0; i < MEMORY; i += 16) {
 		output << setfill('0') << setw(3) << right << hex << i << " | ";
-		//printf("%03x | ", i);
 		for (int j = i; j < i + 4; j++) {
 			output << setfill('0') << setw(2) << right << hex << (int)this->memory[j];
-			//printf("%02x", this->memory[j]);
 		}
 		output << " ";
-		//printf(" ");
 		for (int j = i + 4; j < i + 8; j++) {
 			output << setfill('0') << setw(2) << right << hex << (int)this->memory[j];
 		}
