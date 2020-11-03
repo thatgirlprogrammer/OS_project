572828794e6ece81a9f38691ef2a159c3724046a
thatgirlprogrammer <mullins.amy@gmail.com>
2020-11-03 09:32:53 -0500

    Update job_number.cpp

--- ConsoleApplication1/ConsoleApplication1/job_number.cpp
+++ ConsoleApplication1/ConsoleApplication1/job_number.cpp
@@ -12,14 +12,6 @@ void OSSim::job_len::sort()
 	std::sort(compare.begin(), compare.end());
 }
 
-void OSSim::job_len::print_compare()
-{
-	for (int i = 0; i < 30; i++) {
-		auto val = compare[i];
-//		std::cout << "job number " << val.get_pcb_info().pc.job_len << std::endl;
-	}
-}
-
 size_t OSSim::job_len::pcb_list_size()
 {
 	return 30;
